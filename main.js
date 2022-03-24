86c977bac538f16d764f7a0f18bf66542f829414
iamgodfortniteubot <73544414+iamgodfortniteubot@users.noreply.github.com>
2022-03-25 00:44:44 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function preload(){
+}
+
+function setup(){
+canvas = createCanvas(300, 300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+
+poseNet = ml5.poseNet(video, modeloaded);
+poseNet.on('pose', got_poses);
+}
+
+function modeloaded(){
+    console.log("posNet is intealised");
+    }
+
+function draw(){
+    image(video, 0, 0, 300, 300); 
+}
+
+function take_snapshot(){
+    save('my_image.png');
+    }
+
+    function got_poses(results){
+        if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = "+ noseX);
+console.log("nose y = "+ noseY);
+        }
+    }
+
